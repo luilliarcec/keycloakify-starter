0187a61f7b1b91f522e748feed9b911a59b15e87
Luis Arce <luilliarcec@gmail.com>
2025-02-27 20:33:00 -0500

    feat: add text link component

---
+++ src/components/text-link.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@/lib/utils';
+import { ComponentProps } from 'react';
+
+type AnchorProps = ComponentProps<'a'>;
+
+export default function TextLink({ className = '', children, ...props }: AnchorProps) {
+    return (
+        <a
+            className={cn(
+                'text-foreground underline decoration-neutral-300 underline-offset-4 transition-colors duration-300 ease-out hover:decoration-current! dark:decoration-neutral-500',
+                className,
+            )}
+            {...props}
+        >
+            {children}
+        </a>
+    );
+}
