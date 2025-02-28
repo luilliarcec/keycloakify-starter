2d733ee8ae7f0144bda5453fc9d80239b35f1630
Luis Arce <luilliarcec@gmail.com>
2025-02-27 20:42:21 -0500

    feat: add translations

--- src/login/i18n.ts
+++ src/login/i18n.ts
@@ -3,7 +3,15 @@ import { i18nBuilder } from "keycloakify/login";
 import type { ThemeName } from "../kc.gen";
 
 /** @see: https://docs.keycloakify.dev/features/i18n */
-const { useI18n, ofTypeI18n } = i18nBuilder.withThemeName<ThemeName>().build();
+const { useI18n, ofTypeI18n } = i18nBuilder
+    .withThemeName<ThemeName>()
+    .withCustomTranslations({
+        en: {
+            noAccount: "Don't have an account?",
+            doRegister: "Sign up",
+        },
+    })
+    .build();
 
 type I18n = typeof ofTypeI18n;
 
