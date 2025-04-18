e548819ed7a16a770b453709359e61cf7d0e018a
Luis Arce <luilliarcec@gmail.com>
2025-04-18 10:17:30 -0500

    feat: wip

--- src/components/password-input.tsx
+++ src/components/password-input.tsx
@@ -1,8 +1,8 @@
 "use client";
 import * as React from "react";
 
-import { cn } from "@/lib/utils";
 import { Eye, EyeOff } from "lucide-react";
+import { Input } from "@/components/ui/input.tsx";
 
 function PasswordInput({ className, ...props }: React.ComponentProps<"input">) {
     const [showPassword, setShowPassword] = React.useState(false);
@@ -10,14 +10,9 @@ function PasswordInput({ className, ...props }: React.ComponentProps<"input">) {
 
     return (
         <div className="relative">
-            <input
+            <Input
                 type={showPassword ? "text" : "password"}
-                className={cn(
-                    "border-input file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
-                    "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
-                    "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
-                    className
-                )}
+                className={className}
                 {...props}
             />
             <div className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-gray-400">
