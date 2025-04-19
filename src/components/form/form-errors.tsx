27e46ace1ab78eb062a27be215298dc6abc95b9a
Luis Arce <luilliarcec@gmail.com>
2025-04-19 14:23:30 -0500

    feat: wip

--- src/components/form/form-errors.tsx
+++ src/components/form/form-errors.tsx
@@ -1,8 +1,9 @@
 import InputError from "@/components/input-error.tsx";
 import { FormInputErrorsProps } from "@/utils/types.ts";
+import { cn } from "@/lib/utils.ts";
 
-export default function FormErrors(props: FormInputErrorsProps) {
-    const { attribute, fieldIndex } = props;
+export default function FormErrors(props: FormInputErrorsProps & { className?: string} ) {
+    const { attribute, fieldIndex, className } = props;
 
     const displayableErrors = props.displayableErrors.filter(
         error => error.fieldIndex === fieldIndex
@@ -16,6 +17,7 @@ export default function FormErrors(props: FormInputErrorsProps) {
         <ul
             id={`input-error-${attribute.name}${fieldIndex === undefined ? "" : `-${fieldIndex}`}`}
             aria-live="polite"
+            className={cn(className)}
         >
             {displayableErrors
                 .filter(error => error.fieldIndex === fieldIndex)
